5bbc64aa0a36f0e8a0b6bea76240b7253feae718
Kleber <ti.klebersantos@gmail.com>
2021-08-29 16:00:17 -0300

    Projeto de banco primeiro commit

---
+++ ProjetoBanco/Enum/TipoConta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBanco.Enum
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+
+        PessoaJuridica = 2
+
+    }
+}
